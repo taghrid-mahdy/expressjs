620961ca5cdcb95770ac645f1aee24f8d8f92a93
taghrid-mahdy <taghrid@pentavalue.com>
2019-12-20 08:29:53 +0200

    first task adding numbers with express

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -6,32 +7,26 @@ const PORT = 3000;
 
 app.listen(PORT, () => console.log(`application is up on ${PORT}`));
 
-// middelware 1
-app.use((req, res, next) => {
-  const data = 'Taghreed on the way';
-  res.locals.taghreed = data;
+app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+  let number = req.body.number;
+  res.locals.number = number + 1
   next();
-})
+});
 
-// middelware 2
-app.use((req, res, next) => {
-  const data = 'abdulazeem on the way';
-  res.locals.abdulazeem = data;
-  const fakeData = { message: 'no data in fakeData' };
-  // next(fakeData);
+app.use((req,res,next)=>{
+  res.locals.number = res.locals.number + 1;
   next();
-})
+});
 
-// handler
-app.use('/', (req, res, next) => {
-  res.json({
-    message: `Server is up at root route! on port ${PORT}`,
-    data: res.locals.taghreed,
-    anotherData: res.locals.abdulazeem,
-  });
-})
+app.use((req,res,next)=>{
+  res.locals.number = res.locals.number + 1;
+  next();
+});
 
-// error handler
-app.use((err, req, res, next) => {
-  res.json({ error: err })
+app.use('/', (req,res,next) => {
+    res.json({
+      number : res.locals.number
+    })
 });
